70ebd7446930c65a0ea8849570656af136d1008f
Terex12 <sz.yufanxu@gmail.com>
2022-08-12 12:31:52 -0600

    clean

--- new-halfhalf-2080/run.sh
+++
@@ -1,3 +0,0 @@
-# old # time python create_hfhf.py 30 10 10 0 2>&1 | tee tttt1.log
-
-time python create_hfhf.py 30 10 10 0 2>&1 | tee newnewAE.log
--- new-halfhalf-v100/run.sh
+++
@@ -1,5 +0,0 @@
-
-## time python create_hfhf.py 30 10 10 0 2>&1 | tee tttt7.log
-
-time python create_hfhf.py 30 30 30 0 2>&1 | tee newnewAE.log
-
